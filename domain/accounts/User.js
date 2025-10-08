6ff61598b4c948b161e1bd4297c07f3181a47162
JRReduta <jrreduta@yahoo.com>
2025-10-08 04:56:10 -0700

    Added User, UserRepo

---
+++ domain/accounts/User.js
@@ -0,0 +1,18 @@
+export default class User {
+  #name;
+
+  #email;
+
+  constructor({ name, email }) {
+    this.#name = name;
+    this.#email = email;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get email() {
+    return this.#email;
+  }
+}
---
+++ domain/interfaces/UserRepo.js
@@ -0,0 +1,48 @@
+import Interface from "./Interface.js";
+import User from "../accounts/User.js";
+
+/**
+ * Repository pattern for stored user info
+ */
+export default class UserRepo extends Interface {
+  constructor() {
+    super();
+  }
+
+  /**
+   * Gets all users
+   * @returns {User[]} all users
+   */
+  async getAllAsync() {}
+
+  /**
+   * Gets a user with a given id
+   * @param {Number} id
+   * @returns {User | null} user with a given id, or null
+   */
+  async getByIdAsync(id) {}
+
+  /**
+   * Adds a user to the db
+   * @param {String} email email
+   * @param {String} name name
+   * @param {String} password password, to be hashed in backend
+   * @returns {User} the new user
+   */
+  async createAsync({ email, name, password }) {}
+
+  /**
+   * Deletes a user from the db w/ the given id
+   * @param {Number} id id
+   * @returns {User | null} the deleted user, or user exists w/ this id
+   */
+  async deleteByIdAsync(id) {}
+
+  /**
+   * ...I plead the 5th. Inspired by {@link https://x.com/hakanshehu/status/1943019056610558432|this}
+   *
+   * @param {*} password password
+   * @returns {Boolean} whether the password does not yet exist in the db
+   */
+  async isPasswordUniqueAsync(password) {}
+}
